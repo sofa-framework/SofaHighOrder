2805c6d43f8173a3579f894a2f5af04be0e136c1
Herve Delingette <Herve.Delingette@inria.fr>
2019-02-20 09:03:10 +0100

    FIX : removed #ifdef SOFA_FLOAT to reflect the new policy to handle both double and float

--- SofaHighOrderFEM/HighOrderTetrahedralDiffusionForceField.cpp
+++ SofaHighOrderFEM/HighOrderTetrahedralDiffusionForceField.cpp
@@ -24,28 +24,16 @@ using namespace sofa::defaulttype;
 
 // Register in the Factory
 int HighOrderTetrahedralDiffusionForceFieldClass = core::RegisterObject("Diffusion on High Order Tetrahedral Mesh")
-#ifndef SOFA_FLOAT
-        .add< HighOrderTetrahedralDiffusionForceField<Vec3dTypes> >()
-        .add< HighOrderTetrahedralDiffusionForceField<Vec2dTypes> >()
-        .add< HighOrderTetrahedralDiffusionForceField<Vec1dTypes> >()
-#endif
-#ifndef SOFA_DOUBLE
-        .add< HighOrderTetrahedralDiffusionForceField<Vec3fTypes> >()
-        .add< HighOrderTetrahedralDiffusionForceField<Vec2fTypes> >()
-        .add< HighOrderTetrahedralDiffusionForceField<Vec1fTypes> >()
-#endif
+        .add< HighOrderTetrahedralDiffusionForceField<Vec3Types> >()
+        .add< HighOrderTetrahedralDiffusionForceField<Vec2Types> >()
+        .add< HighOrderTetrahedralDiffusionForceField<Vec1Types> >()
         ;
 
-#ifndef SOFA_FLOAT
-template class  SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec3dTypes>;
-template class  SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec2dTypes>;
-template class  SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec1dTypes>;
-#endif
-#ifndef SOFA_DOUBLE
-template class SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec3fTypes>;
-template class SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec2fTypes>;
-template class SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec1fTypes>;
-#endif
+
+template class  SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec3Types>;
+template class  SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec2Types>;
+template class  SOFA_HIGHORDER_FEM_API HighOrderTetrahedralDiffusionForceField<Vec1Types>;
+
 
 } // namespace forcefield
 
